76935fdec925c3bb125511e5ce1d5434870bf4d9
KeikoEndo <keiko.endo@sun-asterisk.com>
2020-04-14 14:08:55 +0900

    test commit

---
+++ ConsoleAppSol/ClassLibrary1/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public static string testMethod(string param1)
+        {
+            return param1;
+
+        }
+    }
+}
---
+++ ConsoleAppSol/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using ClassLibrary1;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string msg = Class1.testMethod("test");
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
